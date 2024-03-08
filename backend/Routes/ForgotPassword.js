5f243288dcf8996441a57517d2b79d11db47a4b8
AshifAsh12 <ash604154@gmail.com>
2024-03-08 20:24:48 +0530

    first commit

--- backend/Routes/ForgotPassword.js
+++ backend/Routes/ForgotPassword.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const jstoken = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 module.exports = (db) => {
